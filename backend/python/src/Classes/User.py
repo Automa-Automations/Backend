c52fefbfb761343fbd57dc882b67893224d98cd9
Simon Ferns <business@simonferns.com>
2024-05-09 23:51:10 +0200

    User class full pass (Fully implemented thus far)

--- backend/python/src/Classes/User.py
+++ backend/python/src/Classes/User.py
@@ -1,11 +1,21 @@
 import dataclasses 
 import datetime
-from typing import Optional, BinaryIO
+from typing import Optional, BinaryIO, Any
+from io import BytesIO
+from src.utils import update_value, upload_file, download_file_url, get_value
 import base64
+
+class CannotModifyUserIdException(Exception):
+    """Raised when the programmer attempts to modify the UserID!"""
+    pass
+
+class NotValidFullNameException(Exception):
+    """Raised when the programmer attempts to modify the UserID!"""
+    pass
 
 @dataclasses.dataclass
 class Profile:
-    id: str = dataclasses.field(init=False)
+    id: str
     username: str
     updated_at: datetime.datetime
     full_name: str
@@ -16,10 +26,14 @@ class Profile:
 
     def to_dict(self):
         return dataclasses.asdict(self)
-    
+
+    @staticmethod
+    def from_id(id: str):
+        return Profile(id=id, username="", updated_at=datetime.datetime.now(), full_name="", avatar_url="", website="", expiry_date=datetime.datetime.now(), stripe_customer_id="")
+
 @dataclasses.dataclass
 class DatabaseSyncedProfile():
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         self._id = kwargs.pop('id')
         self._updated_at = kwargs.pop('updated_at')
         self._full_name = kwargs.pop('full_name')
@@ -27,7 +41,6 @@ class DatabaseSyncedProfile():
         self._website = kwargs.pop('website')
         self._expiry_date = kwargs.pop('expiry_date')
         self._stripe_customer_id = kwargs.pop('stripe_customer_id')
-        super().__init__(*args, **kwargs)
 
     @property
     def id(self):
@@ -35,80 +48,94 @@ class DatabaseSyncedProfile():
 
     @id.setter
     def id(self, value):
+        raise CannotModifyUserIdException
         pass # read-only
 
     @property
     def updated_at(self):
+        self._sync()
         return self._updated_at
 
     @updated_at.setter
     def updated_at(self, value):
         if not isinstance(value, datetime.datetime):
-            return # We don't want to set it to an invalid value
+            raise ValueError(f"Cannot have datatype of {type(value)} for value \"updated_at\" Expecting datetime.datetime")
+
+        self._update('updated_at', value, updated_at=False)
         self._updated_at = value
 
     @property
     def full_name(self):
+        self._sync()
         return self._full_name
 
     @full_name.setter
     def full_name(self, value):
-        self._full_name = value
+        if len(value.split(" ")) > 1 and isinstance(value, str):
+            self._update('full_name', value)
+            self._full_name = value
+            return
+
+        raise NotValidFullNameException
 
     @property
     def avatar_url(self):
+        self._sync()
         return self._avatar_url
 
     @avatar_url.setter
     def avatar_url(self, value):
-        """Expects a File Object, Base64 String, or URL"""
-        if not isinstance(value, str):
-            return
-        file_object: Optional[BinaryIO] = None
+        """Expects a File Object, Base64 String, or URL. And must be in png format!"""
         if 'http' in value:
             # Attempt to download it
             # For now we will just download a random image
-            image_url = "https://picsum.photos/200"
-            import requests
-            response = requests.get(image_url)
-            response_bytes = response.content
-            file_object = response_bytes
+            file_object = download_file_url(from_=value)
         elif 'data:image' in value:
             value = value.split(',')[1]
             file_object = base64.b64decode(value)
         elif isinstance(value, BinaryIO):
             file_object = value
+        else:
+            raise ValueError(f"User \"avatar_url\" expect http:// or https:// or base64 or bytes object, but recieved {value} of type {type(value)} instead!")
 
-        supabase_url = "TODO: Upload the file_object"
+        supabase_url = upload_file(bucket_name='avatars', path_on_bucket=f"{self._id}.png", content=file_object)
+        self._update('avatar_url', supabase_url)
         self._avatar_url = supabase_url
 
     @property
     def website(self):
+        self._sync()
         return self._website
 
     @website.setter
     def website(self, value):
         # TODO: Validate the URL using AI reading + Score > 0.5 (Else Null)
+        self._update('website', value)
         self._website = value
 
     @property
     def expiry_date(self):
+        self._sync()
         return self._expiry_date
 
     @expiry_date.setter
     def expiry_date(self, value):
         if not isinstance(value, datetime.datetime):
-            return
+            raise ValueError(f"Cannot have datatype of {type(value)} for value \"expiry_date\" Expecting datetime.datetime")
+
+        self._update('expiry_date', value)
         self._expiry_date = value
     
     @property
     def stripe_customer_id(self):
+        self._sync()
         return self._stripe_customer_id
 
     @stripe_customer_id.setter
     def stripe_customer_id(self, value):
         # TODO: Update all the references in the database
         # TODO: Update the Stripe Customer
+        self._update('stripe_customer_id', value)
         self._stripe_customer_id = value
     
     @staticmethod
@@ -123,4 +150,26 @@ class DatabaseSyncedProfile():
                 key = key[1:]
             output_dict[key] = value
         return output_dict
+    
+    def _update(self, val: str, new_value: Any, updated_at: bool =True):
+        updated_at_now = datetime.datetime.now()
+        table = "profiles"
+        line = self._id
+        val = val
+        line_name = 'id'
 
+        update_value(table=table, line=line, val=val, new_value=new_value, line_name=line_name)
+        if updated_at:
+            update_value(table=table, line=line, val='updated_at', new_value=updated_at_now, line_name=line_name)
+    
+    def _sync(self):
+        new_data = get_value(table='profiles', line=self._id)
+        for key, value in new_data.items():
+            if key.startswith('_'):
+                key = key[1:]
+            setattr(self, f'_{key}', value)
+
+    @staticmethod
+    def from_id(id: str):
+        value = get_value(table='profiles', line=id)
+        return DatabaseSyncedProfile.from_dict(value)
---
+++ backend/python/src/utils.py
@@ -0,0 +1,121 @@
+import sys
+from src.supabase import supabase
+from typing import Any, BinaryIO
+import datetime
+import json
+from io import BytesIO
+import traceback
+import uuid
+import requests
+
+def update_value(table: str, line: str, val: str, new_value: Any, line_name: str = 'id'):
+    try:
+        if isinstance(new_value, datetime.datetime):
+            new_value = str(new_value)
+
+        supabase.table(table).update({val: new_value}).eq(line_name, line).execute()
+    except Exception as e:
+        print(e, traceback.format_exc())
+
+def get_value(table: str, line: str, line_name: str = 'id') -> dict:
+    try:
+        return supabase.table(table).select('*').eq(line_name, line).execute().data[0]
+    except Exception as e:
+        print(e, traceback.format_exc())
+    return {}
+
+
+def upload_file(bucket_name: str, path_on_bucket: str, content: bytes, extended_file_options=None) -> str:
+
+    if extended_file_options is None:
+        extended_file_options = {}
+
+    new_file_name = uuid.uuid4()
+    file_path_segments = path_on_bucket.split('/')
+    filename, extension = file_path_segments[-1].split('.')
+    filename = f"{new_file_name}_{filename}_{datetime.datetime.now().timestamp()}"
+    full_filename = f"{filename}.{extension}"
+    joined_file_segments = "/".join(file_path_segments)
+    if len(joined_file_segments.split('/')) == 1:
+        full_file_path = full_filename
+    else:
+        full_file_path = f"{joined_file_segments}/{full_filename}"
+
+    match extension:
+        # Image MIME Types
+        case "png":
+            file_options = {"content-type": "image/png"}
+        case "jpg":
+            file_options = {"content-type": "image/jpeg"}
+        case "jpeg":
+            file_options = {"content-type": "image/jpeg"}
+        case "gif":
+            file_options = {"content-type": "image/gif"}
+        case "bmp":
+            file_options = {"content-type": "image/bmp"}
+        case "tiff":
+            file_options = {"content-type": "image/tiff"}
+        case "webp":
+            file_options = {"content-type": "image/webp"}
+        case "svg":
+            file_options = {"content-type": "image/svg+xml"}
+
+        # Audio MIME Types
+        case "mp3":
+            file_options = {"content-type": "audio/mpeg"}
+        case "wav":
+            file_options = {"content-type": "audio/wav"}
+        case "ogg":
+            file_options = {"content-type": "audio/ogg"}
+        case "flac":
+            file_options = {"content-type": "audio/flac"}
+        case "aac":
+            file_options = {"content-type": "audio/aac"}
+
+        # Video MIME Types
+        case "mp4":
+            file_options = {"content-type": "video/mp4"}
+        case "avi":
+            file_options = {"content-type": "video/x-msvideo"}
+        case "mov":
+            file_options = {"content-type": "video/quicktime"}
+        case "mkv":
+            file_options = {"content-type": "video/x-matroska"}
+        case "wmv":
+            file_options = {"content-type": "video/x-ms-wmv"}
+
+        # Text MIME Types
+        case "txt":
+            file_options = {"content-type": "text/plain"}
+        case "html":
+            file_options = {"content-type": "text/html"}
+        case "css":
+            file_options = {"content-type": "text/css"}
+        case "csv":
+            file_options = {"content-type": "text/csv"}
+        case "xml":
+            file_options = {"content-type": "text/xml"}
+        case "json":
+            file_options = {"content-type": "application/json"}
+        case "pdf":
+            file_options = {"content-type": "application/pdf"}
+        case "doc":
+            file_options = {"content-type": "application/msword"}
+        case "docx":
+            file_options = {"content-type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document"}
+
+    # TODO: Recursively create the folder if it doesn't exist.
+    # TODO: Create the bucket if it doesn't exist (With full rls lockdown always, because we don't want user interacting with the buckets at all!)
+    # We want that type of functionality because it will allow us to easily bootstrap an entire project.
+    supabase.storage.from_(bucket_name).upload(file=content, path=full_file_path, file_options={**file_options, **extended_file_options})
+    # res = supabase.storage.from_(bucket_name).get_public_url(full_file_path)
+    res = supabase.storage.from_(bucket_name).create_signed_url(full_file_path, sys.maxsize)['signedURL']
+    return res
+
+
+def download_file_url(from_: str) -> BinaryIO:
+    response = requests.get(from_)
+    response_bytes = response.content
+    file_object = response_bytes
+
+    return file_object
