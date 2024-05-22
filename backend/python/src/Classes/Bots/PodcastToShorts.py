13f5cdb8c4741105e9545643f7a2885a3ca467e2
WilliamFerns1 <willfv2@gmail.com>
2024-05-22 13:02:59 +0200

    feat(PodcastToShorts): get_shorts_final_transcripts method made
    This method will use OpenAI to optimize the shorts transcripts by removing all unnecessary sentences, in a way to get the most amount of views.

--- backend/python/src/Classes/Bots/PodcastToShorts.py
+++ backend/python/src/Classes/Bots/PodcastToShorts.py
@@ -2,7 +2,9 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from typing import Optional, List 
 from ollama import Client
 from dotenv import load_dotenv
+from pytube import YouTube
 import os
+import json
 
 load_dotenv()
 
@@ -11,7 +13,7 @@ OLLAMA_HOST_URL=os.getenv("OLLAMA_HOST_URL")
 # import dataclass
 from dataclasses import dataclass
 
-client = Client(OLLAMA_HOST_URL)
+llama_client = Client(OLLAMA_HOST_URL)
 
 @dataclass
 class PodcastToShorts:
@@ -28,6 +30,7 @@ class PodcastToShorts:
 
     def __post_init__(self):
         self.__validate_env_variables()
+        self.yt = YouTube(self.podcast_url)
 
     def get_shorts(self):
         """
@@ -37,7 +40,83 @@ class PodcastToShorts:
         transcriptions_feedback = self.__get_transcripts_feedback(full_sentences_transcript)
         shorts_transcripts = self.__filter_transcripts(transcriptions_feedback)
 
-    def __filter_transcripts(self, transcriptions_feedback, should_make_short = True):
+        # take each short, use OpenAI to remove all unnecessary content in the start and end, so that it is just the short
+        shorts_final_transcripts = self.__get_shorts_final_transcripts(shorts_transcripts)
+        self._generate_shorts(shorts_transcripts)
+
+    def __get_shorts_final_transcripts(self, shorts_transcripts: List[dict]):
+        """
+        Method to get the final transcripts of the shorts, by removing the start and end sentences, to get the optimized short.
+        Parameters:
+        - shorts_transcripts: list: The list of the shorts transcripts
+        Returns: 
+        - list: The list of the final transcripts of the shorts
+        """
+        system_prompt = f"""
+        You get in a transcript, and you are supposed to remove sentences dictionaries in the transcript, so that the transcript only have the part that will be fitting for a short. Make it 100 - 150 words max, and take in the total length into account, as the total length of all sentences dictionaries combined may be at max 55 seconds. The final clip should be in a great format for a social media short, so the primary goal is to make the short get as much views as possible. Here is an example input and output formats:
+        ###
+        Input (the input will be provided to you by the user): 
+        {
+            "transcript": [{'start': 0.0, 'duration': 2.0, 'text': 'sentence 1 text here'}, ...this format goes on for all the sentences],
+            "stats": {
+                "score": 65,
+                "should_make_short": False,
+                "feedback": "The transcript is too long, and the content is not engaging enough."
+            }
+        }
+        Output (you give the output): 
+        {
+            "transcript": [{'start': 0.0, 'duration': 2.0, 'text': 'Hello world!'}], # Change the transcription, to be fitting for a short that will be optimized for the most amount of views. 
+        }
+        """
+        final_transcripts = []
+        for short in shorts_transcripts:
+            message = f"Here is the transcript: {json.dumps(short, indent=4)}"
+            llama_response = llama_client.generate(
+                model=self.llama_model,
+                system=system_prompt,
+                prompt=message,
+                format="json",
+            )["response"]
+
+            append_dict = {
+                "transcript": llama_response["transcript"],
+                "stats": short["stats"]
+            }
+            final_transcripts.append(append_dict)
+
+        return final_transcripts
+
+    def _generate_shorts(self, shorts_transcripts: List[dict]):
+        # for now, just download the podcast and get shorts, unedited.
+        download_response = self.__download_podcast();
+        if download_response["status"] == "success":
+            for short in shorts_transcripts:
+                self._clip_short(short, download_response["output_path"], download_response["filename"])
+        else:
+            raise ValueError(f"Error while downloading the podcast: {download_response['error']}")
+
+    def _clip_short(self, short: dict, output_path: str, filename: str):
+        return
+
+    def __download_podcast(self, output_path: str = "downloads/", filename: str = ""):
+        try:
+            if filename == "":
+                filename = self.yt.title
+
+            self.yt.streams.get_highest_resolution().download(output_path=output_path, filename=filename)
+            return {
+                "output_path": output_path, 
+                "filename": filename, 
+                "status": "success",
+            }
+        except Exception as e:
+            return {
+                "error": e,
+                "status": "error",
+            }
+
+    def __filter_transcripts(self, transcriptions_feedback: List[dict], should_make_short : bool = True):
         """
         Method to filter the transcriptions based on the should_make_short value
         Parameters:
@@ -74,7 +153,7 @@ class PodcastToShorts:
 
         transcripts_feedback = []
         for chunk in chunked_transcript:
-            response = client.generate(
+            response = llama_client.generate(
                 model=self.llama_model,
                 system=system_prompt,
                 prompt=message,
@@ -120,7 +199,7 @@ class PodcastToShorts:
         - list: The list of the formatted transcript
         """
         message = f"Here is the transcript: {transcript}" 
-        return client.generate(
+        return llama_client.generate(
             model=self.llama_model,
             system=system_prompt,
             prompt=message,
