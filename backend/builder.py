962033ffa0cf53997f92d8565346133e4a7d43a0
Simon Ferns <business@simonferns.com>
2024-06-18 17:49:30 +0200

    Wrote the Spec for the last remaining functions

--- backend/builder.py
+++ backend/builder.py
@@ -31,12 +31,56 @@ def build_docker(service_dir_path: str, service: str):
     client.images.build(path=service_dir_path, tag=service.lower())
     return service.lower()
  
+def docker_command_builder(container_name: str, config_path: str):
+    config = json.load(open(config_path))
+
+    if not container_name:
+        container_name = config['base_image']
+
+    run_command = "docker run -d "
+    if "mounts" in config:
+        for (mount_name, mount_path) in config['mounts'].items():
+            run_command += f"-v {mount_name}:{mount_path} "
+
+    if "networking" in config:
+        if "http" in config["networking"]:
+            ports = config['networking']['http'].items()
+            for (internal, external) in ports:
+                run_command += f"-p {external}:{internal} "
+                
+    if "specs" in config:
+        specs = config['specs']
+        if "cpu" in specs:
+            run_command += f"--cpus {specs['cpu']} "
+            
+        if "memory" in specs:
+            run_command += f"--memory {specs['memory']}MB "
+
+        if "gpu" in specs:
+            if "auto" in specs["gpu"]:
+                run_command += "--gpus auto "
+
+    run_command += container_name
+
+    return run_command
+
+
 if __name__ == "__main__":
     services_path = 'services'
     services = list_services(services_path)
     for service in services:
         service_dir_path = os.path.join(services_path, service)
-        if container_type(services_path, service) == ContainerType.DOCKER:
-            build_docker(service_dir_path, service)
-        else:
-            print(f'Invalid container type for {service}')
+        ctype = container_type(services_path, service)
+        container_name = ''
+        if ctype == ContainerType.DOCKER:
+            container_name = build_docker(service_dir_path, service)
+        elif ctype == ContainerType.CONFIG:
+            # We need to use the config.json to ensure everything works (AKA, we use config.json to directly run a container instead of needing to build, but in the future we will make it have fly.io deploy options etc... )
+            print('Config file found, not building anything')
+            
+        config_path = os.path.join(service_dir_path, 'config.json')
+        if not os.path.exists(config_path):
+            print("No config file found, skipping")
+            continue
+
+        os.system(docker_command_builder(container_name, config_path))
