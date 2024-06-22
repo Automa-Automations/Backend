82f266aa65247e3e7a32e47287ad9660e751c0c5
Simon Ferns <business@simonferns.com>
2024-06-22 11:37:43 +0200

    Basic Test Generation Completed

--- backend/python/services/ollama/main.py
+++ backend/python/services/ollama/main.py
@@ -8,5 +8,12 @@ from flask_fs_router import FlaskFSRouter
 app = Flask(__name__)
 FlaskFSRouter(app)
 
+class Hi():
+    def __init__(self):
+        print("THIS IS SOME RANDOM THING")
+
+    def hi(self):
+        print("SAY HI")
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="0.0.0.0")
